9bac05b6b33be15ed07b9c874fac2aa75e148a9d
flibbit31 <98357892+flibbit31@users.noreply.github.com>
2023-01-11 10:31:54 -0700

    Changed name, added different rest file.

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+//roughly followed tutorial from here: https://www.digitalocean.com/community/tutorials/how-to-create-a-web-server-in-node-js-with-the-http-module
+
 const http = require("http");
 
 const requestListener = function (req, res) {
