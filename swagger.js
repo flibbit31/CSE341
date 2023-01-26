01be0461d5989b9be2eb7285aa49efe054fe1af8
flibbit31 <98357892+flibbit31@users.noreply.github.com>
2023-01-26 16:16:51 -0700

    Fixed swagger.js host bug.

--- swagger.js
+++ swagger.js
@@ -4,8 +4,9 @@ const doc = {
     info: {
         title: "Contacts API",
         description: "Contacts are here."
-    }
-    
+    },
+    host: "cse341-j0ct.onrender.com",
+    schemes: ['https']
 };
 
 const output = "./swagger.json";
