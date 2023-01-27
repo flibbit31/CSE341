758126034adb02bf6d7110ec3b70f08970048f50
flibbit31 <98357892+flibbit31@users.noreply.github.com>
2023-01-26 17:02:30 -0700

    Try to make this work with Brother Birch's frontend

--- server.js
+++ server.js
@@ -24,6 +24,18 @@ const port = process.env.PORT || 8080;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+//make this work with Brother Birch's frontend
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Z-Key'
+  );
+  res.setHeader('Content-Type', 'application/json');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  next();    
+});
+
 //set up mongodb
 const { MongoClient } = require('mongodb');
 const ObjectId = require('mongodb').ObjectId;
